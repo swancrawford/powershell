d1883902e7c976b518f84824a52a81fe61dc5798
David Crawford <swancrawford@msn.com>
2022-09-01 16:22:42 +0200

    Node Count Changed to 1

--- Kubernetes/Create_k8s_Cluster.ps1
+++ Kubernetes/Create_k8s_Cluster.ps1
@@ -1,6 +1,6 @@
 az group create --name tacx-k8s-dev --location westeurope
 
-az aks create --resource-group tacx-k8s-dev --name testapp --node-count 2 --generate-ssh-keys --node-vm-size Standard_B2s
+az aks create --resource-group tacx-k8s-dev --name testapp --node-count 1 --generate-ssh-keys --node-vm-size Standard_B2s
 
 az aks get-credentials --resource-group tacx-k8s-dev --name testapp
 
